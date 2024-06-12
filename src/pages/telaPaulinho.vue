dcb0173b4ddb3605ca5f3bcf23affa2431a8d1f5
paulo <paulo.henrich@unochapeco.edu.br>
2024-06-12 20:37:03 -0300

    teste

---
+++ src/pages/telaPaulinho.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+        <v-row>
+            <v-col>
+                <h1>akjshd</h1>
+            </v-col>
+        </v-row>
+    </v-app>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    
+</style>
