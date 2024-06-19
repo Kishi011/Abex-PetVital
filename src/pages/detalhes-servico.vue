80b074625c019c0ed37d6fb6f7e82c0f52f90718
Vinicius Mulinari Stuani <viniciusmulinari.st@gmail.com>
2024-06-19 17:37:38 -0300

    tela de detalhes de serviço pronta

--- src/pages/detalhes-servico.vue
+++ src/pages/detalhes-servico.vue
@@ -34,16 +34,61 @@
 
     <v-row justify="end">
       <v-col cols="2">
-        <v-btn size="x-large">Agendar esse serviço</v-btn>
+        <v-btn size="x-large" @click="dialog = true">Agendar esse serviço</v-btn>
       </v-col>
     </v-row>
 
-    
+    <v-dialog v-model="dialog" width="750">
+      <v-card class="pa-10">
+
+        <v-btn flat small rounded icon="mdi-arrow-left" @click="dialog = false"></v-btn>
+
+        <v-card-title>
+          <v-row>
+            <v-col>
+              <h1>Agendamento</h1>
+              <span>Escolha uma data e horário de sua preferência</span>
+            </v-col>
+          </v-row>
+        </v-card-title>
+
+        <v-card-text class="d-flex justify-center">
+          <div>
+            <v-row>
+              <v-col>
+                <v-date-picker width="350"border></v-date-picker>
+              </v-col>
+            </v-row>
+  
+            <v-row>
+              <v-col>
+                <v-autocomplete label="Horários" hide-details width="350"
+                  :items="['8:30', '9:30', '10:30', '11:30', '13:30', '14:30', '15:30', '16:30']"
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+  
+            <v-row>
+              <v-col class="d-flex justify-end">
+                <v-btn size="large" variant="outlined" to="/pagamento">Agendar</v-btn>
+              </v-col>
+            </v-row>
+          </div>
+        </v-card-text>
+
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'DetalhesServicoPage',
+
+  data() {
+    return {
+      dialog: false,
+    }
+  }
 }
 </script>
